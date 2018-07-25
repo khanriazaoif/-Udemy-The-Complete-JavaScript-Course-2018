891bbb26cbdd4555eb6e21d073889fe46f9a0228
Riaz Khan <khanriazaoif@gmail.com>
2018-07-25 13:54:57 -0500

    Hoisting
    Practice at hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,17 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+calculateAge(1965);
+
+function calculateAge(year){
+    console.log(2016-year);
+}
+
+
+// retirement(1979);
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
 
 
+// variables
 
+var age = 23;
+console.log(age);
 
-
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
